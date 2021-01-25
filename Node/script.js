f837b93f073fc01549496b81161fcbaa7f76ca4e
Timofeev Evgeniy <e.timofeev@devinotele.com>
2021-01-25 17:04:51 +0300

    getBeeToken

--- Node/script.js
+++ Node/script.js
@@ -1,27 +1,38 @@
-let http = require('http');
+let http = require("http");
+let beeAuth = require("./beeAuth");
 
 let goHome = (req, res) => {
-  console.log('home');
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.end(JSON.stringify({ name: 'John', surname: 'Doe' }));
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.end(JSON.stringify({ name: "John", surname: "Doe" }));
 };
 
 let goSome = (req, res) => {
   res.writeHead(200);
-  res.end('This is some page');
+  res.end("This is some page");
+};
+
+let getBeeToken = async (req, res) => {
+  let data = await beeAuth.getBeeToken();
+
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.end(JSON.stringify(data));
 };
 
 let server = http.createServer((req, res) => {
   switch (req.url) {
-    case '/':
+    case "/":
       goHome(req, res);
       break;
-    case '/some':
+    case "/some":
       goSome(req, res);
+      break;
+    case "/get-bee-token":
+      getBeeToken(req, res);
       break;
   }
 });
 
 server.listen(8001);
-//fetch('http://127.0.0.1:8001').then(response => response.json()).then(data =>console.log(data));
+fetch('http://127.0.0.1:8001/get-bee-token').then(response => response.json()).then(data =>console.log(data));
